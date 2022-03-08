48a49217b4d6494d2b0b7747f40d04212a057339
noemipintaldi <noemipintaldi@gmail.com>
2022-03-08 17:13:40 +0100

    esercizio completato

---
+++ myscript.js
@@ -0,0 +1,6 @@
+let nomeUtente = prompt ("Qual è il tuo nome?", "noemi")
+
+let cognomeUtente = prompt ("Qual è il tuo cognome?", "pintaldi")
+
+let coloreUtente = prompt ("Qual è il tuo colore preferito?", "azzurro")
+
